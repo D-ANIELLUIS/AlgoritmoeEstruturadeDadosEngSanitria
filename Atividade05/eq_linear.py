56cb543d524b971ed09642e91e9b302005b472c1
Daniel Luis de Melo Pinheiro <161645155+D-ANIELLUIS@users.noreply.github.com>
2024-09-06 17:06:56 -0300

    Atividade 5, test.py
    Foi feito uma estrutura para validar se o código de equação linear está correto de acordo com os parâmetros definidos no arquivo test_eq_linear.

---
+++ Atividade05/eq_linear.py
@@ -0,0 +1,6 @@
+#função linear com TDD em python
+def funcao_linear(x):
+    #f(x)=2x
+    return 2 * x
+
+#print(funcao_linear(2))
---
+++ Atividade05/test_eq_linear.py
@@ -0,0 +1,20 @@
+import unittest
+import eq_linear
+
+class TestFuncaoLinear(unittest.TestCase):
+    def test_valor_positivo(self):
+        #teste para um valor positivo
+        #de X
+        resultado = eq_linear.funcao_linear(2)
+        self.assertEquals(resultado, 4)
+
+    def test_valor_negativo(self):
+        #Teste para um valor negativo
+        #de X
+        resultado = eq_linear.funcao_linear(-2)
+        self.assertEqual(resultado, -4)
+
+    def test_valor_zero(self):
+        #Teste para x igual a 0
+        resultado = eq_linear.funcao_linear(0)
+        self.assertEqual(resultado, 0)
